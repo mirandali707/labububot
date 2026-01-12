1afdc82e819e2e1eda0eda73ffeed13d1d6f0d87
Miranda <miranda@Mirandas-MacBook-Air-2.local>
2026-01-12 15:24:55 +0800

    renamed BLESetup.cpp to lower snake case for consistency

--- firmware/ble_test_ino/ble_test_ino.ino
+++ firmware/ble_test_ino/ble_test_ino.ino
@@ -5,7 +5,7 @@
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-#include <BLESetup.h>
+#include <ble_setup.h>
 
 uint32_t value = 0;
 
